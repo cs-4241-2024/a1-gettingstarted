0beb9f7cb1f1644834e0a9dd6dfedb0f32a2b1cb
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-08-22 19:56:09 -0400

    completed all task and subtask

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/style.css':
+      sendFile(response, 'style.css')
+      break
+    case '/copyemailscript.js':
+      sendFile(response, 'copyemailscript.js')
+      break
     default:
-      response.end( '404 Error: File Not Found' )
+      response.writeHead(404, { 'Content-Type': 'text/plain' });
+      response.end('404 Error: File Not Found');
   }
 })
 
