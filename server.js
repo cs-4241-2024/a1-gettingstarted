1270f7c196c5ef98be2dfec77a16ec7666527de6
nrogerson234 <160359018+nrogerson234@users.noreply.github.com>
2024-08-26 15:15:36 -0400

    Nick Rogerson project 1 Webware, 1st ver

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+    fs   = require('fs'),
+    port = 3000
 
-const server = http.createServer( function( request,response ) {
+const server = http.createServer( function( request, response ) {
   switch( request.url ) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
+      break
+    case '/styles.css':
+      sendFile( response, 'styles.css', 'text/css' )
+      break
+    case '/script.js':
+      sendFile( response, 'script.js', 'application/javascript' )
       break
     default:
       response.end( '404 Error: File Not Found' )
@@ -17,8 +23,14 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function( response, filename, contentType ) {
+  fs.readFile( filename, function( err, content ) {
+    if (err) {
+      response.writeHead(404, { 'Content-Type': 'text/plain' })
+      response.end('404 Error: File Not Found')
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType })
+      response.end( content, 'utf-8' )
+    }
+  })
 }
