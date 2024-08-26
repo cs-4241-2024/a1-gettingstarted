7c695092b376986635ae80bda2bc826dda88db01
Eddie Stump <ejstump13@gmail.com>
2024-08-26 17:09:19 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-import { createServer } from "http";
-import { readFile } from "fs";
-const port = 3000;
+const http = require("http"),
+  fs = require("fs"),
+  port = 3000;
 
-const server = createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
   switch (request.url) {
     case "/":
       sendFile(response, "index.html");
       break;
-    case "/index.html":
-      sendFile(response, "index.html");
-      break;
-    case "/style.css":
-      sendFile(response, "style.css");
-      break;
     default:
-      response.end("404 Error: File Not Found");
+      sendFile(response, request.url.slice(1));
+      break;
   }
 });
 
 server.listen(process.env.PORT || port);
 
 const sendFile = function (response, filename) {
-  readFile(filename, function (err, content) {
-    response.end(content, "utf-8");
+  fs.readFile(filename, function (err, content) {
+    if (err) {
+      response.end("404 Error: File Not Found");
+    } else {
+      response.end(content, "utf-8");
+    }
   });
 };
