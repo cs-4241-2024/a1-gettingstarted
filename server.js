02b81da09c49dadab3e02764e346934c7c1df93f
Eddie Stump <ejstump13@gmail.com>
2024-08-26 12:13:31 -0400

    Prettier and linting fixes, emptied README

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+import { createServer } from "http";
+import { readFile } from "fs";
+const port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+const server = createServer(function (request, response) {
+  switch (request.url) {
+    case "/":
+      sendFile(response, "index.html");
+      break;
+    case "/index.html":
+      sendFile(response, "index.html");
+      break;
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end("404 Error: File Not Found");
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
-}
+const sendFile = function (response, filename) {
+  readFile(filename, function (err, content) {
+    response.end(content, "utf-8");
+  });
+};
