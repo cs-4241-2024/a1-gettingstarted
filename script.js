a323fc252f23640ad87a10e3e83462f240fb08d5
Nandita <74115831+mknandita@users.noreply.github.com>
2024-08-27 02:35:49 -0400

    got my animated blob(s, hopefully eventually) moving under my static info blob:)

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ const palette = ["#AFF04D", "#71E649", "#D2D950", "#F0E24D", "#E6C849"]
 
 var id = null;
 window.onload = function () { //need window.onload so that you call the element *after* it's loaded
-    var c = document.getElementById("top-left");
-    var ctx = c.getContext("2d");
+    //setup
+    var draw_blob = function(which_color, how_big, radius, context){
+        context.beginPath();
+        context.strokeStyle =  which_color;
+        context.lineWidth = how_big;
+        context.arc(0, 0, radius, 0, 2 * Math.PI);
+        context.stroke();
+    }
+   
+    //static info blob
+    var static = document.getElementById("static-canvas");
+    var info = static.getContext("2d");
+
+    draw_blob("blue", 25, 100, info); //ok its working but it's getting cleared
+
+    //animated blobs
+    var animation = document.getElementById("animation-canvas");
+    var anim = animation.getContext("2d");
     var size = 0;
     var which_color = 1; //index of current color from palette array, skipping index 0 which is the background
     var how_big = 5; 
 
-    clearInterval(id);
+    //clearInterval(id);
 
     id = setInterval(frame, 25);
     function frame() {
-    if (size == 45) {
+    if (size == 450) {
         //clearInterval(id); //stop animation
-        ctx.clearRect(0, 0, 1000, 1000);
+        anim.clearRect(0, 0, 1000, 1000);
         size = 0;
         how_big = (Math.random() * 25) + 5; //line weight between 5 and 25
         if (which_color != palette.length){
@@ -25,15 +41,11 @@ window.onload = function () { //need window.onload so that you call the element 
             which_color = 1; //reset if at end of array
         }
     } else {
-        ctx.clearRect(0, 0, 1000, 1000);
+        anim.clearRect(0, 0, 1000, 1000);
         //console.log(c.style.width); this is an empty string -- why? even if changed to pxls in style.css
-        //ctx.clearRect(c.style.left, c.style.top, c.style.width, c.style.height);
+        //anim.clearRect(c.style.left, c.style.top, c.style.width, c.style.height);
         size++;
-        ctx.beginPath();
-        ctx.strokeStyle = palette[which_color];
-        ctx.lineWidth = how_big;
-        ctx.arc(0, 0, size, 0, 2 * Math.PI);
-        ctx.stroke();
+        draw_blob(which_color, how_big, size, anim); 
     }
   }
 
