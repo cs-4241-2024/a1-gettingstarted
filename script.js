7682c183732bf62f249cfca03fb3158deecbc887
Eddie Stump <ejstump13@gmail.com>
2024-08-28 13:52:35 -0400

    Added element racing functionality to website

---
+++ script.js
@@ -0,0 +1,34 @@
+// Get racing elements
+const raceElements = [];
+for (let element of document.getElementsByClassName("racer")) {
+  raceElements.push({ position: 0, velocity: 0, element: element });
+}
+
+// Function that executes one frame of the race
+const raceStep = () => {
+  let raceOver = false;
+
+  for (let racer of raceElements) {
+    // Velocity is changed randomly, must stay above 0
+    racer.velocity = Math.max(racer.velocity + Math.random() - 0.5, 0);
+    racer.position += racer.velocity;
+    racer.element.style = "margin-left: " + racer.position + "px";
+
+    // If the racer is close enough to the right side of the screen, it wins
+    if (
+      racer.element.getBoundingClientRect().right >=
+      document.body.clientWidth - 20
+    ) {
+      racer.element.style.background = "green";
+      raceOver = true;
+      break;
+    }
+  }
+
+  // If the race is still going, step another frame
+  if (!raceOver) requestAnimationFrame(raceStep);
+};
+
+// Start race on button click
+document.getElementsByTagName("button")[0].onclick = () =>
+  requestAnimationFrame(raceStep);
