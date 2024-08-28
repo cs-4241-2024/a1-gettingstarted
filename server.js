f40e5e3c8b651db0a58742efbcfaa8963121b9a3
SupermanBritt <57573715+SupermanBritt@users.noreply.github.com>
2024-08-28 05:23:03 -0400

    Initial Commit With My Changes

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+  fs = require('fs'),
+  port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
       break
+    // default:
+    //   response.end('404 Error: File Not Found')
     default:
-      response.end( '404 Error: File Not Found' )
+      const name = request.url.slice(1)
+      sendFile(response, name)
+      break
   }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function (response, filename) {
+  fs.readFile(filename, function (err, content) {
+    response.end(content, 'utf-8')
+  })
 }
