fab1126d3d02538f567fdfbbfa5f888ce6ba7bef
Liswerd <dragonwerdo4714@gmail.com>
2024-08-28 09:35:50 -0400

    Changed color of title text, made background fluctuate back and forth instead of randomly

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ window.onload = function () {
     let green = 0;
     let blue = 0;
     let rotation = 10;
+    let topValue = false;
     const fnc = function () {
       //console.log("test", red);
-      document.body.style =
-        "background-color:rgb(" + red + "," + green + ",  255)";
-      document.getElementById("funnyImage").style.transform =
-        "rotate(" + rotation + "deg)";
-      red++;
+      document.body.style = "background-color:rgb(" + red + "," + green + ",  255)";
+      document.getElementById("funnyImage").style.transform = "rotate(" + rotation + "deg)";
+      if(topValue == false)
+      {
+        red++;
+        green++;
+        if(red == 255)
+        {
+          topValue = true; 
+        }
+      }
+      else{
+        topValue = true;
+        red--;
+        green--;
+      }
       rotation = rotation + 2;
-      green = green + 2;
+      if(red == 0)
+      {
+        topValue = false;
+      }
       //if 
       //blue = blue + 30;
       setTimeout(fnc, 50);
-      red = red % 255;
-      green = green % 255;
+      //red = red % 255;
+      //green = green % 255;
       //blue = blue % 255;
     };
     setTimeout(fnc, 20);
