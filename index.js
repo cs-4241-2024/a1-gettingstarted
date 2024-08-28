4eebee59de94c2e6d4d3445b20f5580a9cbd9cc0
SophiaJohnn <sophiasunnyjohn@gmail.com>
2024-08-28 16:55:53 -0400

    fixed error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const rotated = document.getElementById("rotated");
+// const rotated = document.getElementById("rotated");
 
-let rotation = 0;
-const angle = 90;
+// let rotation = 0;
+// const angle = 90;
 
-function rotateImage()
-{
-  rotation = (rotation + angle)%360;
-  rotated.style.transform = `rotate(${rotation}deg)`;
-}
+// function rotateImage()
+// {
+//   rotation = (rotation + angle)%360;
+//   rotated.style.transform = `rotate(${rotation}deg)`;
+// }
 
 // const rotated = document.getElementById("rotated");
 // function rotateImage()
